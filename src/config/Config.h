ca9c736a2a34a91e534f33d761137c7d819eca97
DeFiDude <59237470+DeFiDude@users.noreply.github.com>
2026-03-20 19:43:09 -0600

    v1.6.4: Bump version for LXMF bidirectional messaging fix

--- src/config/Config.h
+++ src/config/Config.h
@@ -6,8 +6,8 @@
 
 #define RATDECK_VERSION_MAJOR  1
 #define RATDECK_VERSION_MINOR  6
-#define RATDECK_VERSION_PATCH  3
-#define RATDECK_VERSION_STRING "1.6.3"
+#define RATDECK_VERSION_PATCH  4
+#define RATDECK_VERSION_STRING "1.6.4"
 
 // --- Feature Flags ---
 #define HAS_DISPLAY     true
